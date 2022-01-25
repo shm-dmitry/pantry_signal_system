2fb5f7868fa1b235255520a634b1fe6138872fa6
dmitry <shm-dmitry@yandex.ru>
2022-01-25 15:06:07 +0300

    Supply controller schematics and firmware ready

--- avr/logic-module/supply_api.ino
+++ avr/logic-module/supply_api.ino
@@ -1,10 +1,30 @@
 #define SUPPLY_AVR_ENABLE_PIN 5
 
-#define SUPPLY_AVR_MAX_AWAIT_TIME 1000
+#define SUPPLY_AVR_MAX_AWAIT_TIME 3000
+
+#define SUPPLY_AVR_PRINT_CALIBRATION false
 
 #define HEADER_1  0x12
 #define HEADER_2  0x13
 #define HEADER_3  0x14
+
+#define SUPPLY_AVR_READ_UINT8(store_to, error) { \
+    if (!supply_api_read_byte(&store_to)) { \
+      return error; \
+    } \
+ \
+    calculated_crc += store_to; \
+  }
+
+#define SUPPLY_AVR_READ_UINT16(store_to, error) { \
+    uint8_t high; \
+    uint8_t low; \
+ \
+    SUPPLY_AVR_READ_UINT8(high, error); \
+    SUPPLY_AVR_READ_UINT8(low, error); \
+ \
+    store_to = high * 0xFF + low; \
+  }
 
 SoftwareSerial supply(6, 7);
 
@@ -19,74 +39,53 @@ uint8_t supply_api_read(ModuleData * data) {
   digitalWrite(SUPPLY_AVR_ENABLE_PIN, LOW);
   
   // await for a supply module start up
-  delay(30);
+//  delay(30);
 
   if (!supply_api_check_header()) {
     return 1;
   }
 
+  uint16_t battery1_analog_input;
   uint8_t battery1_voltage_x10;
   uint8_t battery1_percent;
+  uint16_t battery2_analog_input;
   uint8_t battery2_voltage_x10;
   uint8_t battery2_percent;
+  uint16_t battery3_analog_input;
   uint8_t battery3_voltage_x10;
   uint8_t battery3_percent;
+  uint8_t active_battery_value;
   uint8_t active_battery;
 
-  if (!supply_api_read_byte(&battery1_voltage_x10)) {
-    return 2;
-  }
+  uint8_t calculated_crc = 0;
 
-  if (!supply_api_read_byte(&battery1_percent)) {
-    return 3;
-  }
-
-  if (!supply_api_read_byte(&battery2_voltage_x10)) {
-    return 4;
-  }
-
-  if (!supply_api_read_byte(&battery2_percent)) {
-    return 5;
-  }
-
-  if (!supply_api_read_byte(&battery3_voltage_x10)) {
-    return 6;
-  }
-
-  if (!supply_api_read_byte(&battery3_percent)) {
-    return 7;
-  }
-
-  uint8_t active_battery_high;
-  uint8_t active_battery_low;
-  if (!supply_api_read_byte(&active_battery_high)) {
-    return 11;
-  }
-  if (!supply_api_read_byte(&active_battery_low)) {
-    return 12;
-  }
-
+  SUPPLY_AVR_READ_UINT16(battery1_analog_input, 1);
+  SUPPLY_AVR_READ_UINT8(battery1_voltage_x10, 2);
+  SUPPLY_AVR_READ_UINT8(battery1_percent, 3);
+  SUPPLY_AVR_READ_UINT16(battery2_analog_input, 4);
+  SUPPLY_AVR_READ_UINT8(battery2_voltage_x10, 5);
+  SUPPLY_AVR_READ_UINT8(battery2_percent, 6);
+  SUPPLY_AVR_READ_UINT16(battery3_analog_input, 7);
+  SUPPLY_AVR_READ_UINT8(battery3_voltage_x10, 8);
+  SUPPLY_AVR_READ_UINT8(battery3_percent, 9);
+  SUPPLY_AVR_READ_UINT8(active_battery_value, 10);
+  SUPPLY_AVR_READ_UINT8(active_battery, 11);
+  
+#if SUPPLY_AVR_PRINT_CALIBRATION
   Serial.print("active battery level ");
-  Serial.println(active_battery_high * 256 + active_battery_low);
-
-  if (!supply_api_read_byte(&active_battery)) {
-    return 8;
-  }
+  Serial.print(active_battery_value);
+  Serial.print("; B1: ");
+  Serial.print(battery1_analog_input);
+  Serial.print("; B2: ");
+  Serial.print(battery2_analog_input);
+  Serial.print("; B3: ");
+  Serial.println(battery3_analog_input);
+#endif
 
   uint8_t crc;
   if (!supply_api_read_byte(&crc)) {
     return 9;
   }
-
-  uint8_t calculated_crc = battery1_voltage_x10;
-  calculated_crc += battery1_percent;
-  calculated_crc += battery2_voltage_x10;
-  calculated_crc += battery2_percent;
-  calculated_crc += battery3_voltage_x10;
-  calculated_crc += battery3_percent;
-  calculated_crc += active_battery_high;
-  calculated_crc += active_battery_low;
-  calculated_crc += active_battery;
 
   if (calculated_crc != crc) {
     return 10;
@@ -112,12 +111,11 @@ bool supply_api_check_header() {
     return false;
   }
 
-  if (temp != HEADER_1) {
-    Serial.print("H1 ERR ");
-    Serial.print(temp, HEX);
-    Serial.print(" / ");
-    Serial.println(HEADER_1, HEX);
-    return false;
+  if (temp == 0) {
+    // I dont know, wtf, but periodically first readed byte from stream is zero...
+    if (!supply_api_read_byte(&temp)) {
+      return false;
+    }
   }
 
   if (!supply_api_read_byte(&temp)) {
--- avr/supply/supply.ino
+++ avr/supply/supply.ino
@@ -1,57 +1,165 @@
 #define PRIMARY_BATTERY_LEVEL     A3
 #define RESERVE_BATTERY_LEVEL     A2
 #define RESERVE_5V_LEVEL          A1
-#define BATTERY_RESERVE_INUSE     A0
-#define BATTERY_RESERVE_5V_INUSE  1
+#define BATTERY_BATTERY_INUSE     1
 
-#define PRIMARY_ACTUAL            127
-#define PRIMARY_ONDISPLAY         41
+typedef struct t_measure_vector_data {
+  uint8_t fromvalue;
+  uint16_t koef;
+} t_measure_vector_data;
+
+// Calibration coefs depends on actual resistance value you setup in schematics.
+
+// *** How to calibrate voltage level measurement? ***
+// 1. cleanup measure vectors:
+// const t_measure_vector_data primary_level[] = {};
+// 2. upload scetch with empty vector to ATTINY
+// 3. enable SUPPLY_AVR_PRINT_CALIBRATION in logic-module/supply-api.ino
+// 4. connect laboratory power supply to primary supply
+// 5. setup power 6-7V on power supply and check serial output from logic-module.
+// You will see lines like a
+// active battery level XXX; B1: XXX; B2: YYYYY; B3: ZZZZZ
+// 6. write B1 value to excel column A and actual supply voltage to excel column B
+// 7. increase voltage on power supply on 1-2 volts and repeat point 6-7
+// 8. continue untill voltage will increased to 14-15 volts
+// 9. calculate KOEF using excel formula C = A * 100 / B 
+// 10. calculate FROMVALUE using excel formula D = A / 10
+// 11. write values to measure vector data in source code
+// 12. repeat from point 4, but connect power supply to reserve supply and use B2 value at step 6
+// 13. repeat from point 4, but connect power supply to reserve 5V supply and use B3 value at step 6
+// 14. flush firmware with measure vectors to ATTINY. You will see actual voltage level
+// --- Example ---
+// voltage == 12.34 volt
+// logic-module showed value 821 at point 6
+// So: 
+// .fromvalue = 82
+// .koef = 6653
+
+// *** How to calibrate battery percent usage? ***
+// 1. process voltage level calibration as described below.
+// 2. get absolute maximum value from your power supplies
+// 3. multiply it on 10
+// 4. write it into PRIMARY_MAX_LEVEL_X10, RESERVE_MAX_LEVEL_X10 and RESERVE_5V_MAX_LEVEL_X10
+// --- Example ---
+// If absolute maximum voltage of your primary supply is 12.3 volts, you have to write in source code
+// #define PRIMARY_MAX_LEVEL_X10 123
+
+// *** Calibration of current battery usage ***
+// 1. enable SUPPLY_AVR_PRINT_CALIBRATION in logic-module/supply-api.ino
+// 2. connect power supply to RESERVE_5V supply only
+// 3. check serial output from logic-module.
+// You will see lines like a
+// active battery level XXX; B1: XXX; B2: YYYYY; B3: ZZZZZ
+// 4. write active battery level value somewere
+// 5. repeat 2-4 , but connect supply to PRIMARY supply only
+// 6. repeat 2-4 , but connect supply to RESERVE supply only
+// 7. Check stored values from 4, 5 and 6. Check is conditions below is correct? If values is incorrect - check resistance you used in schematics.
+// 7.1 Value#4 < Value#5
+// 7.2 Value#5 < Value#6
+// 8. get any value between Value#4 and Value#5, and store it in
+// #define THRESHOLD_INUSE_PRIMARY   50
+// 9. get any value between Value#5 and Value#6, and store it in
+// #define THRESHOLD_INUSE_RESERVE   90
+// 10. flush firmware to ATTINY. You will see actual power supply in use
+
+const t_measure_vector_data primary_level[] = {
+    {
+       .fromvalue = 0,
+       .koef = 7100
+    },
+    {
+       .fromvalue = 55,
+       .koef = 6804
+    },
+    {
+       .fromvalue = 67,
+       .koef = 6590
+    },
+    {
+       .fromvalue = 90,
+       .koef = 6485
+    }
+  };
+const t_measure_vector_data reserve_level[] = {
+   {
+       .fromvalue = 0,
+       .koef = 7278
+    },
+    {
+       .fromvalue = 50,
+       .koef = 6455
+    },
+    {
+       .fromvalue = 67,
+       .koef = 6288
+    },
+    {
+       .fromvalue = 77,
+       .koef = 6234
+    }
+  };
+const t_measure_vector_data reserve5v_level[] = {
+   {
+       .fromvalue = 0,
+       .koef = 7100
+    },
+    {
+       .fromvalue = 55,
+       .koef = 6804
+    },
+    {
+       .fromvalue = 67,
+       .koef = 6590
+    },
+    {
+       .fromvalue = 90,
+       .koef = 6485
+    }
+  };
+
 #define PRIMARY_MAX_LEVEL_X10     144
-
-#define RESERVE_ACTUAL            127
-#define RESERVE_ONDISPLAY         40
 #define RESERVE_MAX_LEVEL_X10     120
-
-#define RESERVE_5V_ACTUAL         1
-#define RESERVE_5V_ONDISPLAY      1
 #define RESERVE_5V_MAX_LEVEL_X10  120
 
-#define THRESHOLD_INUSE_RESERVE   800
+#define THRESHOLD_INUSE_RESERVE   90
+#define THRESHOLD_INUSE_PRIMARY   50
 
 #define HEADER_1  0x12
 #define HEADER_2  0x13
 #define HEADER_3  0x14
 
+#define SERIAL_WRITE(v) { \
+  uint8_t serial_write_byte = (v); \
+  crc += serial_write_byte; \
+  Serial.write(serial_write_byte); \
+}
+
 uint8_t crc = 0;
 
 void setup() {
+  Serial.begin();
+
   pinMode(PRIMARY_BATTERY_LEVEL,   OUTPUT);
   pinMode(RESERVE_BATTERY_LEVEL,   OUTPUT);
   pinMode(RESERVE_5V_LEVEL,        OUTPUT);
-  pinMode(BATTERY_RESERVE_5V_INUSE,OUTPUT);
-//  pinMode(BATTERY_RESERVE_INUSE,   OUTPUT);
+  pinMode(BATTERY_BATTERY_INUSE,   OUTPUT);
 
   digitalWrite(PRIMARY_BATTERY_LEVEL,    LOW);
   digitalWrite(RESERVE_BATTERY_LEVEL,    LOW);
   digitalWrite(RESERVE_5V_LEVEL,         LOW);
-  digitalWrite(BATTERY_RESERVE_5V_INUSE, LOW);
-//  digitalWrite(BATTERY_RESERVE_INUSE,    LOW);
+  digitalWrite(BATTERY_BATTERY_INUSE,    LOW);
 
   delay(10);
 
   pinMode(PRIMARY_BATTERY_LEVEL,   INPUT);
   pinMode(RESERVE_BATTERY_LEVEL,   INPUT);
   pinMode(RESERVE_5V_LEVEL,        INPUT);
-  pinMode(BATTERY_RESERVE_INUSE,   INPUT);
-  pinMode(BATTERY_RESERVE_5V_INUSE,INPUT);
 
   delay(100);
-
-  Serial.begin();
 }
 
 void loop() {
-  delay(10); // await some time before send data
+  delay(100); // await some time before send data
 
   crc = 0;
 
@@ -61,9 +169,9 @@ void loop() {
   Serial.write(HEADER_3);
 
   // write data
-  readAndSendBatteryLevel(PRIMARY_BATTERY_LEVEL, PRIMARY_ACTUAL,    PRIMARY_ONDISPLAY,    PRIMARY_MAX_LEVEL_X10);
-  readAndSendBatteryLevel(RESERVE_BATTERY_LEVEL, RESERVE_ACTUAL,    RESERVE_ONDISPLAY,    RESERVE_MAX_LEVEL_X10);
-  readAndSendBatteryLevel(RESERVE_5V_LEVEL,      RESERVE_5V_ACTUAL, RESERVE_5V_ONDISPLAY, RESERVE_5V_MAX_LEVEL_X10);
+  readAndSendBatteryLevel(PRIMARY_BATTERY_LEVEL, primary_level,    sizeof(primary_level)/sizeof(t_measure_vector_data),    PRIMARY_MAX_LEVEL_X10);
+  readAndSendBatteryLevel(RESERVE_BATTERY_LEVEL, reserve_level,    sizeof(reserve_level)/sizeof(t_measure_vector_data),    RESERVE_MAX_LEVEL_X10);
+  readAndSendBatteryLevel(RESERVE_5V_LEVEL,      reserve5v_level,  sizeof(reserve5v_level)/sizeof(t_measure_vector_data),  RESERVE_5V_MAX_LEVEL_X10);
   readAndSendCurrentBatteryNumber();
 
   // write CRC
@@ -74,39 +182,62 @@ void loop() {
 }
 
 void readAndSendCurrentBatteryNumber() {
-  uint16_t value = analogRead(BATTERY_RESERVE_INUSE);
-  crc += value / 256;
-  Serial.write(value / 256);
-  crc += value % 256;
-  Serial.write(value % 256);
+  // power-up capacitor
+  pinMode(BATTERY_BATTERY_INUSE,   OUTPUT);
+  digitalWrite(BATTERY_BATTERY_INUSE, HIGH);
 
-  if (value >= THRESHOLD_INUSE_RESERVE) {
+  delay(50);
+
+  // power down and measure discharge time...
+  unsigned long start = millis();
+  pinMode(BATTERY_BATTERY_INUSE,   INPUT);
+  while(digitalRead(BATTERY_BATTERY_INUSE) == HIGH);
+  
+  uint8_t passed = (uint8_t) (millis() - start);
+
+  SERIAL_WRITE(passed);
+
+  uint8_t value;
+
+  if (passed >= THRESHOLD_INUSE_RESERVE) {
     value = 2;
-  } else if (digitalRead(BATTERY_RESERVE_5V_INUSE) == HIGH) {
-    value = 3;
-  } else {
+  } else if (passed >= THRESHOLD_INUSE_PRIMARY) {
     value = 1;
+  } else {
+    value = 3;
   }
 
-  crc += value;
-  Serial.write(value);
+  SERIAL_WRITE(value);
 }
 
-void readAndSendBatteryLevel(analog_pin_t pin, uint16_t actual, uint16_t ondisplay, uint8_t maxLevel_x10) {
+void readAndSendBatteryLevel(analog_pin_t pin, const t_measure_vector_data* vector, uint8_t vector_size, uint8_t maxLevel_x10) {
   uint16_t level_value = analogRead(pin);
   if (level_value < 200) {
     level_value = 0;
   }
-  //Serial.write(level_value / 100);
-  //crc += level_value/100;
-  //Serial.write(level_value % 100);
-  //crc += level_value%100;
-  //return;
-  uint8_t level = (((uint32_t) level_value) * ((uint32_t) actual) * 50) / 1023 / ondisplay;
-  crc += level;
-  Serial.write(level);
-  level = ((uint16_t)level * 100) / maxLevel_x10;
-//  level = OSCCAL;
-  crc += level;
-  Serial.write(level);
+
+  // calibration raw data
+  SERIAL_WRITE(level_value / 0xFF);
+  SERIAL_WRITE(level_value % 0xFF);
+
+  uint8_t level = 0xFF;
+
+  if (vector_size > 0) {
+    vector_size--;
+    while(true) {
+      if (level_value >= vector[vector_size].fromvalue * 10) {
+        level = (uint8_t) (((uint32_t)level_value * (uint32_t)1000) / (uint32_t)vector[vector_size].koef);
+        break;
+      }
+      
+      if (vector_size == 0) {
+        break;
+      }
+      
+      vector_size--;
+    }
+  }
+
+  SERIAL_WRITE(level);
+  SERIAL_WRITE(((uint16_t)level * 100) / maxLevel_x10);
 }
